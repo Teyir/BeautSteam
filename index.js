9e6c2aed94cb6d8b18bbf45bf4c8de51f265e6d1
Teyir <60007549+Teyir@users.noreply.github.com>
2021-09-29 10:25:42 +0200

    [IMPROVE] comments

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+//Import
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-
+//Déclaration du client (bot)
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+//Log de la connexion du bot
 client.once('ready', () => {
     console.log('Bot connecté !');
 });
 
-
+//Intégration du token pour la connexion du bot
 client.login(token);

d959fe50a6f6fd161fbd35a7a3117a096aec0440
Axel BAUDOIN <axel.baudoin@epsi.fr>
2021-09-29 11:57:45 +0200

    discord

---
+++ deploy-commands.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('./config.json');
+
+const dotenv = require('dotenv').config();
+
+const commands = [
+    new SlashCommandBuilder().setName('axeping').setDescription('Replies with pong !'),
+    new SlashCommandBuilder().setName('axeserver').setDescription('Replies with server info !'),
+    new SlashCommandBuilder().setName('axeuser').setDescription('Replies with user info!'),
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const { Client, Intents } = require('discord.js');
 const { guildId } = require('./config.json');
 const dotenv = require('dotenv').config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
 
 client.once('ready', () => {
     console.log('Bot connecté !');
 });
 
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'axeping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'axeserver') {
+        await interaction.reply('Server info.');
+    } else if (commandName === 'axeuser') {
+        await interaction.reply('User info.');
+    }
+});
 
 client.login(process.env.TOKEN);
